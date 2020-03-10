66d0fd2278af798edc2019513b6244cbe538f0de
d1jxtra <dardom03@gmail.com>
2020-03-10 13:03:46 +0100

    Initial commit

---
+++ test.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+double f(double x,double y)
+{
+    return x/y;
+}
+
+int main()
+{
+    double x,y;
+    cout<<"Unesi 2 broja: ";
+    cin>>x>>y;
+    cout<<f(x,y)<<endl;
+    return 0;
+}
